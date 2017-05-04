13ffd41f941bf86c33a209bcb8982f4f546e3ed8
Robby Costales <rcostales15@simons-rock.edu>
2017-05-04 00:30:21 -0400

    Working backprop (somewhat). Need to fix a few of the paths

---
+++ hard_coded_xor.py
@@ -0,0 +1,212 @@
+# Author: Robert Costales
+# Language: Python 3
+# Date: 4/3/2017
+# Summary: This program is a hard-coded MLP with one hidden layer
+#		   The weights are trained on XOR
+
+
+import random
+import math
+import copy
+
+# _-_-_-_-_-_-_-_-_-_-_-_ SETTINGS _-_-_-_-_-_-_-_-_-_-_-_ #
+
+NUM_ITER = 10000  # number of times to train through set
+GAMMA = 1  # learning rate
+FREQ = 20    # frequency of printing values
+
+# _-_-_-_-_-_-_-_-_-_-_-_ WEIGHTS _-_-_-_-_-_-_-_-_-_-_-_ #
+
+# x1, x2 are inputs
+# a, b, c, d are hidden layer nodes
+# p is output
+
+# weights for a
+w_a0 = random.random()  # threshold
+w_a1 = random.random()  # for x1 feed
+w_a2 = random.random()  # for x2 feed
+
+# weights for b
+w_b0 = random.random()  # threshold
+w_b1 = random.random()  # for x1 feed
+w_b2 = random.random()  # for x2 feed
+
+# weights for c
+w_c0 = random.random()  # threshold
+w_c1 = random.random()  # for x1 feed
+w_c2 = random.random()  # for x2 feed
+
+# weights for d
+w_d0 = random.random()  # threshold
+w_d1 = random.random()  # for x1 feed
+w_d2 = random.random()  # for x2 feed
+
+# weights for p
+w_p0 = random.random()  # threshold
+w_p1 = random.random()  # for a feed
+w_p2 = random.random()  # for b feed
+w_p3 = random.random()  # for c feed
+w_p4 = random.random()  # for d feed
+
+weights = [[w_p0, w_p1, w_p2, w_p3, w_p4], [w_a0, w_a1, w_a2], [w_b0, w_b1, w_b2], [w_c0, w_c1, w_c2], [w_d0, w_d1, w_d2]]
+
+# _-_-_-_-_-_-_-_-_-_-_-_ TRAINING  _-_-_-_-_-_-_-_-_-_-_-_ #
+
+# xor training set:
+xor_set = [[0, 0, 0],
+           [0, 1, 1],
+           [1, 0, 1],
+           [1, 1, 0]]
+
+for i in range(NUM_ITER):
+    local_errors = []
+    if i % FREQ == 0:
+        prev_weights = copy.deepcopy(weights)
+        weights = [[w_p0, w_p1, w_p2, w_p3, w_p4], [w_a0, w_a1, w_a2], [w_b0, w_b1, w_b2], [w_c0, w_c1, w_c2],
+                   [w_d0, w_d1, w_d2]]
+
+        print("{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}"
+              "{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}   ".format(*weights[0], *weights[1], *weights[2], *weights[3], *weights[4]), end="")
+        del_weights = copy.deepcopy(weights)
+        for i in range(len(weights)):
+            for j in range(len(weights[i])):
+                del_weights[i][j] = weights[i][j] - prev_weights[i][j]
+        print("{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}"
+              "{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}{:<8.3f}  ".format(*del_weights[0], *del_weights[1], *del_weights[2], *del_weights[3],
+                                                                  *del_weights[4]))
+    for j in xor_set:
+        x1 = j[0]       # first input
+        x2 = j[1]       # second inputs
+        target = j[2]   # expected output (target)
+
+        # FEED FORWARD
+
+        # hidden nodes
+        net_a = (w_a0 * 1) + (w_a1 * x1) + (w_a2 * x2)
+
+        out_a = 1 / (1 + math.exp(0 - net_a))
+
+        net_b = (w_b0 * 1) + (w_b1 * x1) + (w_b2 * x2)
+        out_b = 1 / (1 + math.exp(0 - net_b))
+
+        net_c = (w_c0 * 1) + (w_c1 * x1) + (w_c2 * x2)
+        out_c = 1 / (1 + math.exp(0 - net_c))
+
+        net_d = (w_d0 * 1) + (w_d1 * x1) + (w_d2 * x2)
+        out_d = 1 / (1 + math.exp(0 - net_d))
+
+        # p node
+        net_p = (w_p0 * 1) + (w_p1 * out_a) + (w_p2 * out_b) + (w_p3 * out_c) + (w_p4 * out_d)
+        out_p = 1 / (1 + math.exp(0 - net_p))
+
+        # error
+        error = (0.5 * (target - out_p) ** 2)
+        local_errors.append(error)
+
+
+        # DELTA RULE (for output weights)
+        # output weights
+        del_w_p1 = (out_p - target) * out_p * (1 - out_p) * out_a
+        w_p1 -= GAMMA * del_w_p1
+
+        del_w_p2 = (out_p - target) * out_p * (1 - out_p) * out_b
+        w_p2 -= GAMMA * del_w_p2
+
+        del_w_p3 = (out_p - target) * out_p * (1 - out_p) * out_c
+        w_p3 -= GAMMA * del_w_p3
+
+        del_w_p4 = (out_p - target) * out_p * (1 - out_p) * out_d
+        w_p4 -= GAMMA * del_w_p4
+
+        w_p0 -= GAMMA * ((out_p - target) * out_p * (1 - out_p) * 1)
+
+        # HIDDEN LAYER UPDATES
+        # a weights
+
+        path = (out_p - target) * out_p * (1 - out_p)
+        # sets path value to make code easier to read
+
+        del_w_a0 = path * w_p1 * out_a*(1 - out_a) * 1
+        w_a0 -= GAMMA * del_w_a0
+
+        del_w_a1 = path*w_p1 * out_a*(1 - out_a) * x1
+        w_a1 -= GAMMA * del_w_a1
+
+        del_w_a2 = path*w_p1 * out_a*(1 - out_a) * x2
+        w_a2 -= GAMMA * del_w_a2
+
+        # b weights
+        del_w_b0 = path* w_p2 * out_b * (1 - out_b) * 1
+        w_b0 -= GAMMA * del_w_b0
+
+        del_w_b1 = path* w_p2 * out_b * (1 - out_b) * x1
+        w_b1 -= GAMMA * del_w_b1
+
+        del_w_b2 = path* w_p2 * out_b * (1 - out_b) * x2
+        w_b2 -= GAMMA * del_w_b2
+
+        # c weights
+        del_w_c0 = path* w_p3 * out_c * (1 - out_c) * 1
+        w_c0 -= GAMMA * del_w_c0
+
+        del_w_c1 = path* w_p3 * out_c * (1 - out_c) * x1
+        w_c1 -= GAMMA * del_w_c1
+
+        del_w_c2 = (out_p - target) * out_p * (1 - out_p) * w_p3 * out_c * (1 - out_c) * x2
+        w_c2 -= GAMMA * del_w_c2
+
+        # d weights
+        del_w_d0 = path * w_p4 * out_d * (1 - out_d) * 1
+        w_d0 -= GAMMA * del_w_d0
+
+        del_w_d1 = path * w_p4 * out_d * (1 - out_d) * x1
+        w_d1 -= GAMMA * del_w_d1
+
+        del_w_d2 = path * w_p4 * out_d * (1 - out_d) * x2
+        w_d2 -= GAMMA * del_w_d2
+
+
+# TESTING
+print("\nTESTING\n")
+
+print("{0:<8}{1:<8}{2:<8}{3:<8}{4:<8}\n".format("Inp 1", "Inp 2", "Exp", "Act", "Status", "Num"))
+for j in xor_set:
+    weights = [[w_p0, w_p1, w_p2, w_p3, w_p4], [w_a0, w_a1, w_a2], [w_b0, w_b1, w_b2], [w_c0, w_c1, w_c2],
+               [w_d0, w_d1, w_d2]]
+    x1 = j[0]
+    x2 = j[1]
+    target = j[2]
+
+    # FEED FORWARD
+
+    # hidden nodes
+    net_a = (w_a0 * 1) + (w_a1 * x1) + (w_a2 * x2)
+    out_a = 1 / (1 + math.exp(0 - net_a))
+
+    net_b = (w_b0 * 1) + (w_b1 * x1) + (w_b2 * x2)
+    out_b = 1 / (1 + math.exp(0 - net_b))
+
+    net_c = (w_c0 * 1) + (w_c1 * x1) + (w_c2 * x2)
+    out_c = 1 / (1 + math.exp(0 - net_c))
+
+    net_d = (w_d0 * 1) + (w_d1 * x1) + (w_d2 * x2)
+    out_d = 1 / (1 + math.exp(0 - net_d))
+
+    # p node
+    net_p = (w_p0 * 1) + (w_p1 * out_a) + (w_p2 * out_b) + (w_p3 * out_c) + (w_p4 * out_d)
+    out_p = 1 / (1 + math.exp(0 - net_p))
+
+    if round(target, 1) == round(out_p, 1):
+        status = "Success"
+    else:
+        status = "Fail"
+    print("{0:<8}{1:<8}{2:<8}{3:<8}{4:<8} {5:<8}".format(x1, x2, target, round(out_p, 2), status, out_p))
+
+print("\nEl Fin")
+
+print([[w_p0, w_p1, w_p2, w_p3, w_p4], [w_a0, w_a1, w_a2], [w_b0, w_b1, w_b2], [w_c0, w_c1, w_c2],
+               [w_d0, w_d1, w_d2]])
+
+waits = [[[], []], [[w_a1, w_a2, w_a0], [w_b1, w_b2, w_b0], [w_c1, w_c2, w_c0], [w_d1, w_d2, w_d0]], [[w_p1, w_p2, w_p3, w_p4, w_p0]]]
+
+print("\n\n{0}".format(waits))
--- main.py
+++ main.py
@@ -4,17 +4,47 @@
 # File Summary: Main file containing the neural network framework, as well as
 #               the specifications of any created neural networks
 
-# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ IMPORT STATEMENTS _-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+# IMPORT STATEMENTS ###########################################################
 
 import numpy as np
-import math
-import random as r
-import copy as c
+import copy
+import random
 
-# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ CLASSES _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+# CLASSES #####################################################################
+
+
+def dot(a, b):
+    s = 0
+    for i in range(len(a)):
+        s += a[i] * b[i]
+    return s
+
+
+def step(a):
+    if a < 0:
+        return -1
+    elif 0 <= a < 0.5:
+        return 0
+    elif 0.5 <= a <= 1:
+        return 1
+    else:
+        return -1
+
+
+def sigmoid(inp):
+    """
+    sigmoid function
+
+    Args:
+        inp: from signal
+    Returns:
+        sigmoid of input
+    """
+    return 1 / (1 + np.exp(-inp))
+
 
 class Network:
-    def __init__ (self, dimensions, activation_type="adaline"):
+    def __init__(self, dimensions, learning_rate=1.0, activation_type="adaline"):
         """
         Initializes network
         1. Creates nodes based on activation_type
@@ -31,46 +61,222 @@ class Network:
         # creates class attribute of dimensions
         self.dimensions = dimensions
         self.avn_type = activation_type
+        # creates bias values
+        self.bias = 1
+        # learning rate
+        self.gamma = learning_rate
 
         # CREATES NODE LIST
-        self.node_list = []
+        self.nodes = []
 
         # initializes input nodes
-        self.num_inputs = len(dimensions[0])
+        self.num_inputs = self.dimensions[0]
         inputs = []
         for i in range(self.num_inputs):
-            inputs.append(Node(self.avn_type, "input", [], []))
-        self.node_list.append(inputs)
+            inputs.append(Node(activation_type=self.avn_type,
+                               layer_type="input"))
+        self.nodes.append(inputs)
 
         # initializes hidden layer nodes
         self.num_hidden_layers = len(self.dimensions) - 2
         for i in range(self.num_hidden_layers):
             layer = []
             for j in range(self.dimensions[i+1]):
-                layer.append(Node(self.avn_type, "hidden", [], [])) ###########
+                layer.append(Node(activation_type=self.avn_type,
+                                  layer_type="hidden"))
+            self.nodes.append(layer)
 
         # initializes output nodes
-        self.num_outputs = len(dimensions[-1])
+        self.num_outputs = self.dimensions[-1]
+        outputs = []
+        for i in range(self.num_outputs):
+            outputs.append(Node(activation_type=self.avn_type,
+                                layer_type="output"))
+        self.nodes.append(outputs)
 
-    def feedforward (self):
+        # CREATES WEIGHT LIST
+        # weights[layer][node][specific weights corresponding to bias +
+        # previous nodes]
+        self.weights = []
+        # value all weights will be initialized to
+        weight_init_val = 0.5
+
+        # inputs do not have weights (to left), so we use empty sub-lists
+        input_weights = []
+        for i in range(self.num_inputs):
+            input_weights.append([])
+        self.weights.append(input_weights)
+
+        # all net weights
+        for layer in range(1, self.num_hidden_layers + 2):
+            layer_weights = []
+            for node in range(len(self.nodes[layer])):
+                node_weights = []
+                # take all previous nodes into account +1 for the bias
+                for i in range(len(self.nodes[layer-1])+1):
+                    node_weights.append(copy.deepcopy(random.random()))
+                # some for loop here to add to node_weights
+                layer_weights.append(copy.deepcopy(node_weights))
+            self.weights.append(copy.deepcopy(layer_weights))
+
+    def __str__(self):
+        return "include specs here (will update later)"
+
+    def feedforward(self, inputs):
         """
-        Feedforward of neural network
+        Feed-forward of neural network
 
         Returns:
             outputs (in a list)
         """
+        # number of inputs should match number of input nodes
+        if len(inputs) != self.num_inputs:
+            # raise "error"
+            print("feed-forward: error regarding number of inputs")
+            return []
+        else:
+            # set all outputs of input nodes to "inputs" list
+            for i in range(self.num_inputs):
+                self.nodes[0][i].output = inputs[i]
 
-    def backprop (self):
+        # find all other nodes' outputs
+        for layer in range(1, len(self.nodes)):
+            for node in range(len(self.nodes[layer])):
+                weights = self.weights[layer][node][:]
+                values = [j.output for j in self.nodes[layer-1]]
+                values += [self.bias]
+                self.nodes[layer][node].output = sigmoid(dot(weights, values))
+
+        outputs = [j.output for j in self.nodes[-1]]
+        # print(outputs)
+        return outputs
+
+    def backprop(self, t):
         """
-        Updating weights of neural network through backpropagation
+        Updating weights of neural network through back-propagation
 
         Returns:
             void
-
         """
 
+        # DELTA RULE
+        # paths in form: (path value, origin node)
+        # origin node used for weight indexing for next layer
+
+        # only one "old" path at start
+        old_paths = [1]
+        # to fill during next loop
+        new_paths = []
+
+        # for each node in the last layer
+        for node in range(len(self.weights[-1])):
+            for path in old_paths:
+                # for each weight connecting the nodes in the previous layer to
+                # the current node
+                out_pi = self.nodes[-1][node].output
+                new_paths.append((path * (out_pi - t[node]) * out_pi * (1 - out_pi), node))
+                for weight in range(len(self.weights[-1][node])):
+                    # if it is the last weight, it corresponds to the bias
+                    if weight == len(self.weights[-1][node]) - 1:
+                        out_xi = self.bias
+                    else:
+                        out_xi = self.nodes[-2][weight].output
+
+                    # we find out delta
+                    dw = path*(out_pi - t[node]) * out_pi * (1 - out_pi) * out_xi
+                    # update the weights
+                    self.weights[-1][node][weight] -= self.gamma * dw
+
+        # all other weights
+        for layer in range(len(self.weights)-2, 0, -1):
+            old_paths = []
+            old_paths += new_paths
+            new_paths = []
+            for node in range(len(self.nodes[layer])):
+                for path in old_paths:
+                    out_p = self.nodes[layer][node].output
+                    w_p = self.weights[layer+1][path[1]][node]
+
+                    # create new path for each new node * old_path
+                    new_paths.append((path[0] * w_p * out_p * (1 - out_p), node))
+
+                    # for each weight connecting the nodes in the previous
+                    # layer to the current node
+                    for weight in range(len(self.weights[layer][node])):
+                        # if it is the last weight, it corresponds to the bias
+                        if weight == len(self.weights[layer][node]) - 1:
+                            # equals bias on left
+                            out_x = self.bias
+                        else:
+                            # equals node on left that weight is connected to
+                            out_x = self.nodes[layer-1][weight].output
+
+                        # we find out delta
+                        dw = path[0] * w_p * out_p * (1-out_p) * out_x
+                        # update the weights
+                        self.weights[layer][node][weight] -= self.gamma * dw
+
+    def training(self, iterations, inputs, targets):
+        """
+        Feeds inputs forward, compares to targets, uses back-propagation to
+        update weights
+        Supervised learning
+
+        Args:
+            iterations : number of training iterations
+            inputs : data input values
+            targets: corresponding target values
+
+        Returns:
+            void
+        """
+        # print(self.weights)
+        for i in range(iterations):
+            for n in range(len(inputs)):
+                # updates output values of each node
+                self.feedforward(inputs[n])
+
+                # updates weights - uses back-propagation
+                self.backprop(targets[n])
+                # print(self.weights)
+
+    def testing(self, inputs, targets):
+        """
+        Prints all sorts of fun stuff
+
+        :param inputs:
+        :param targets:
+        :return: void
+        """
+
+        # title
+        for j in range(len(targets[0])):
+            node_number = "N#"
+            raw = "Raw"
+            rounded = "Rnd"
+            target = "Tgt"
+            status = "Sts"
+            print("   {0:<4}{1:<8}{2:<4}{3:<4}{4:<6}".format(node_number, raw, rounded, target, status), end="    ")
+        print("\n")
+
+        # actual input results
+        for i in range(len(targets)):
+            self.feedforward(inputs[i])
+            for j in range(self.num_outputs):
+                node_number = j
+                raw = "{0:.3f}".format(self.nodes[-1][j].output)
+                target = targets[i][j]
+                rounded = step(float(raw))
+                if rounded == target:
+                    status = "{0}".format(True)
+                else:
+                    status = "{0}".format(False)
+                print("   {0:<4}{1:<8}{2:<4}{3:<4}{4:<6}".format(node_number, raw, rounded, target, status), end="    ")
+            print(" ")
+        print(" ")
+
 class Node:
-    def __init__ (self, activation_type, layer_type, prev_nodes, next_nodes):
+    def __init__(self, activation_type, layer_type):
         """
         Initializes Node
 
@@ -83,39 +289,50 @@ class Node:
 
         """
         self.activation_type = activation_type
-        self.layer_type = layer_type
-        self.prev_nodes = prev_nodes
-        # initializes random weights corresponding to each input node
-        self.weights = [random.random() for i in range(len(prev_nodes))]
-        self.next_nodes = next_nodes
+        # output after activation function
         self.output = 0
-
-    def get_signal (self):
-        """
-        obtains signal from previous nodes to be passed through an activation
-        function
-
-        Returns:
-            dot product of weights and previous node outputs
-
-        """
-        return numpy.dot(self.weights, [i.output for i in self.prev_nodes])
-
-    def sigmoid (self, inp):
-        """
-        sigmoid function
-
-        Args:
-            inp: from signal
-        Returns:
-            sigmoid of input
-        """
-        return 1/(1 + np.exp(-inp))
-
-    def output (self):
-        x = self.get_signal()
-        self.output = self.sigmoid(x)
-        return self.output
+        self.layer_type = layer_type
 
 
-net_a = [2, 4, 1]
+# Initialize data sets ########################################################
+
+bin_set = [[0, 0],
+           [0, 1],
+           [1, 0],
+           [1, 1]]
+
+and_target = [[0],
+              [0],
+              [0],
+              [1]]
+
+xor_target = [[0],
+              [1],
+              [1],
+              [0]]
+
+xorplus_target = [[0, 1],
+                  [1, 0],
+                  [1, 0],
+                  [0, 1]]
+
+andplus_target = [[0, 1],
+                  [0, 1],
+                  [0, 1],
+                  [1, 0]]
+
+
+# print("\n3 Layer 'XOR' and '!XOR' Check:\n")
+#
+# xor_net = Network([2, 4, 2], learning_rate=.3)
+#
+# xor_net.training(4000, bin_set, xorplus_target)
+# xor_net.testing(inputs=bin_set, targets=xorplus_target)
+
+
+print("\n3 Layer 'AND' and '!AND' Check:\n")
+
+xor_net = Network([2, 4, 2], learning_rate=4)
+
+xor_net.training(5000, bin_set, plus_target)
+xor_net.testing(inputs=bin_set, targets=andplus_target)
